d95e1b7ed8dab405f8fc7178e310214acfb8204c
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-11-28 01:03:27 +0700

    index: Integrate city data with the form
    This allows the user to choose the city via select input with
    search feature.
    
    Co-authored-by: Muhammad Fitrah Pandjalu <pandjalu9j@gmail.com>
    Signed-off-by: Muhammad Fitrah Pandjalu <pandjalu9j@gmail.com>
    Co-authored-by: Taufiq Pohan <m.taufiq30s@hotmail.com>
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>
    Co-authored-by: Nauvalsa Yanandana <yuliananda013@gmail.com>
    Signed-off-by: Nauvalsa Yanandana <yuliananda013@gmail.com>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>VNL Member Attendance Form</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/select2.min.css"/>
 </head>
 <body>
 	<div class="container">
@@ -18,10 +19,8 @@
 					<input type="text" class="form-control" id="phone_number"/>
 				</div>
 				<div class="mb-3">
-					<label for="phone_number" class="form-label">City</label>
-					<select class="form-select" aria-label="Default select example">
-						<option value="" selected>Open this select menu</option>
-					</select>
+					<label for="city" class="form-label">City</label>
+					<select id="city" class="form-control"></select>
 				</div>
 				<div class="mb-3">
 					<label for="email" class="form-label">Email address</label>
@@ -31,6 +30,22 @@
 			</form>
 		</div>
 	</div>
+	<script type="text/javascript" src="assets/js/jquery-3.6.1.min.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/select2.full.min.js"></script>
+	<script type="text/javascript">
+		function load_select2_city(data)
+		{
+			$("#city").select2({
+				data: data.results
+			});
+		}
+
+		$.ajax({
+			url: "assets/regions.json",
+			dataType: "json",
+			success: load_select2_city
+		});
+	</script>
 </body>
 </html>
